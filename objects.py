562f9fdd90880fb3dc97b0eef6bd6a3d2e6f2032
yobleck <sennewell@gmail.com>
2021-10-31 12:41:30 -0400

    fix rotation remove old rotation code

--- objects.py
+++ objects.py
@@ -5,14 +5,14 @@ class Bone():
     # bone coordinates are in the format ((x,y),(x,y)) or (Joint,(x,y))
     # TODO decorations
     def __init__(self, coor, thicc=None):
-        print("bone created")
+        #print("bone created")
         if type(coor[0]) is tuple:
             self.parent_joint = Joint(coor[0])
         elif type(coor[0]) is Joint:
             self.parent_joint = coor[0]
         self.child_joint = Joint(coor[1])
         self.length = math.dist(self.parent_joint.pos, self.child_joint.pos)
-        print(self.length)
+        #print(self.length)
         self.children = []  # Bones
         self.thicc = thicc or 1  # deal with pass by reference?
 
@@ -22,54 +22,30 @@ class Bone():
         self.children.append( Bone( (self.child_joint, end_coor), thicc ) )
         
     def rotate(self, angle=None, anchor_joint=None, mouse_pos=None):  # mouse_pos is temporary
-        if not angle and mouse_pos:
-            #mouse_pos = ()  # function to get mouse_pos from xlib
-            angle = math.atan2((self.parent_joint.pos[1]-mouse_pos[1]), (self.parent_joint.pos[0]-mouse_pos[0]))
-            print(angle)
-            #rot_cw = True
         if not anchor_joint:
             anchor_joint = self.parent_joint
-        #TODO rotation is wrong. need to take into account clockwise vs counter clockwise
+        
         temp_child_joint_rel_origin = (self.child_joint.pos[0]-anchor_joint.pos[0], self.child_joint.pos[1]-anchor_joint.pos[1])
+        
+        if not angle and mouse_pos:
+            #mouse_pos = ()  # TODO function to get mouse_pos from Xlib
+            #angle = math.atan2((self.parent_joint.pos[1]-mouse_pos[1]), (self.parent_joint.pos[0]-mouse_pos[0]))
+            # zero the two points rel to origin then do cosine rule
+            temp_mouse_pos_rel_origin = (mouse_pos[0]-anchor_joint.pos[0], mouse_pos[1]-anchor_joint.pos[1])
+            a = math.dist((0,0), temp_child_joint_rel_origin)
+            b = math.dist((0,0), temp_mouse_pos_rel_origin)
+            c = math.dist(temp_child_joint_rel_origin, temp_mouse_pos_rel_origin)
+            angle = math.acos( (a**2+b**2-c**2)/(2*a*b) )
+            #print(angle, math.atan2(temp_child_joint_rel_origin[1],temp_child_joint_rel_origin[0]),
+                  #math.atan2(temp_mouse_pos_rel_origin[1],temp_mouse_pos_rel_origin[0]))
+        
+        #TODO rotation is wrong? need to take into account clockwise vs counter clockwise
+        
         after_rotation = rot_cw(temp_child_joint_rel_origin, angle)
         self.child_joint.move_abs((after_rotation[0]+anchor_joint.pos[0], after_rotation[1]+anchor_joint.pos[1]))
         if self.children:
             for c in self.children:
                 c.rotate(angle=angle, anchor_joint=anchor_joint)
-
-    """TODO DELETE THIS LATER
-    def mouse_rotate(self, mouse_pos):
-        #scratch all the below.
-        #get the angle the same way
-        #then just calculate shifting all the points to the origin then use the equations in the stackoverflow post
-        #then undo the origin shifting
-        #(think of all the child points as a rotating squareish spiral
-        
-        #https://stackoverflow.com/questions/20104611/find-new-coordinates-of-a-point-after-rotation
-        #https://www.khanacademy.org/math/geometry/hs-geo-transformations/hs-geo-rotations/a/rotating-shapes
-        #pass
-        slope = (self.parent_joint.pos[1]-mouse_pos[1])/(self.parent_joint.pos[0]-mouse_pos[0])
-        angle = math.atan2((self.parent_joint.pos[1]-mouse_pos[1]), (self.parent_joint.pos[0]-mouse_pos[0]))
-        new_x = self.length * math.cos(angle)
-        new_y = self.length * math.sin(angle)
-        new_x2 = -self.child_joint.pos[1]*math.sin(angle) + self.child_joint.pos[0]*math.cos(angle)
-        new_y2 = self.child_joint.pos[1]*math.cos(angle) - self.child_joint.pos[0]*math.sin(angle)
-        print("out:", mouse_pos, slope, angle, new_x, new_y, new_x2, new_y2)
-        #old_child_pos = self.child_joint.pos
-        #self.child_joint.pos = (self.parent_joint.pos[0]-new_x, self.parent_joint.pos[1]-new_y)
-        self.child_joint.move_abs( (self.parent_joint.pos[0]-new_x, self.parent_joint.pos[1]-new_y) )
-        if self.children:
-            for c in self.children:
-                pass
-                #c._rotate(angle, self.parent_joint.pos)
-                #c._translate((new_x, new_y))
-    
-    def _rotate(self, angle, og_bone_origin):
-        dist_from_origin = math.sqrt( (self.child_joint.pos[0]-og_bone_origin[0])**2 + (self.child_joint.pos[1]-og_bone_origin[1])**2 )
-        new_x = dist_from_origin * math.cos(angle)
-        new_y = dist_from_origin * math.sin(angle)
-        self.child_joint.move_abs( (self.parent_joint.pos[0]-new_x, self.parent_joint.pos[1]-new_y) )
-        """
 
     def _translate(self, delta):
         # Never call this directly on a Bone. only call through Head/parent Bone
@@ -84,7 +60,7 @@ class Head(): #bone
         self.pos = coor
         self.radius = radius or 5
         self.child_joint = Joint((self.pos[0],self.pos[1]+self.radius))
-        self.thicc = thicc or 1  # TODO fill or just border
+        self.thicc = thicc or 1  # TODO fill or border only option
         self.children = []
 
     def add_child(self, end_coor, thicc=None):
@@ -102,7 +78,7 @@ class Head(): #bone
 class Joint():
     # coor is a 2-tuple (x,y)
     def __init__(self, coor):
-        print("joint created")
+        #print("joint created")
         self.pos = coor
 
     def move_rel(self, delta):
@@ -127,10 +103,11 @@ class Scene():
 #https://stackoverflow.com/questions/20104611/find-new-coordinates-of-a-point-after-rotation
 #https://en.wikipedia.org/wiki/Rotation_matrix
 #https://www.mathsisfun.com/algebra/trig-cosine-law.html
+#https://stackoverflow.com/questions/1211212/how-to-calculate-an-angle-from-three-points
 def rot_cw(pos, angle):
     return (pos[1]*math.sin(angle) + pos[0]*math.cos(angle),
             pos[1]*math.cos(angle) - pos[0]*math.sin(angle))
 
-def rot_ccw(pos, angle):
+def rot_ccw(pos, angle):  # NOTE is this still needed
     return (pos[0]*math.cos(angle) - pos[1]*math.sin(angle),
             pos[0]*math.sin(angle) + pos[1]*math.cos(angle))
--- x.py
+++ x.py
@@ -1,1 +1,10 @@
 import Xlib 
+
+"""
+buttons on top
+layers on right side
+frames on bottom
+
+button click animation after action runs
+snap scrolling for frames/layers
+"""
