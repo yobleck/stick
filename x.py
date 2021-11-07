c93a80d3dd7fc52ce5b2e8852b66a75b70d408c8
yobleck <sennewell@gmail.com>
2021-11-06 20:11:50 -0400

    some ui notes and ignore temp ui file

--- x.py
+++ x.py
@@ -5,6 +5,13 @@ buttons on top
 layers on right side
 frames on bottom
 
+project file path as text input above buttons
+
+status(black)/error(red)/success(green) messages at very bottom
+
 button click animation after action runs
 snap scrolling for frames/layers
+
+buttons:
+    load project?, save frame, delete frame, insert frame, render video
 """
